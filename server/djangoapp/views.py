60114fbac9a0b6591662c9c6e4ad7b4afb9f86ca
SSUbhedar14 <sharvarisubhedar2000@gmail.com>
2024-11-01 03:43:25 -0400

    Changes for dealer

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -91,16 +91,7 @@ def get_dealerships(request, state="All"):
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
     return JsonResponse({"status":200,"dealers":dealerships})
-# def get_dealerships(request):
-# ...
-
-# Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
-
-# Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-# ...
+    
 def get_dealer_details(request, dealer_id):
     if(dealer_id):
         endpoint = "/fetchDealer/"+str(dealer_id)
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,12 +1,15 @@
 import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register"; // Import Register component
 import { Routes, Route } from "react-router-dom";
-
+import Dealer from "./components/Dealers/Dealer"
+import Dealers from './components/Dealers/Dealers';
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />,
-      <Route path="/register" element={<Register />} /> 
+      <Route path="/register" element={<Register />} />,
+      <Route path="/dealers" element={<Dealers/>} />,
+      <Route path="/dealer/:id" element={<Dealer/>} />
     </Routes>
   );
 }
